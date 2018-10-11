56bcecc563245563b847d9055d4340f3ee6211da
Hp <Hp@DESKTOP-SSU3LF0>
2018-10-11 10:56:05 +0530

    First Maven14 commit

---
+++ mvn14Project/src/test/java/mvn14Project/DcTest.java
@@ -0,0 +1,18 @@
+package mvn14Project;
+
+import org.testng.annotations.Test;
+
+public class DcTest 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("test2");
+	}
+
+}
